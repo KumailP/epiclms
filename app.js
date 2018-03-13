39e522fb3759d52b63947c83cab01b392dcf20a3
KumailP <kumailpirzada@gmail.com>
2018-03-13 23:16:57 +0500

    Completed login/signup session system

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ var bodyParser = require('body-parser');
 var lessMiddleware = require('less-middleware');
 const expressValidator = require('express-validator');
 
+// authentication
+var session = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var MySQLStore = require('express-mysql-session')(session);
+var bcrypt = require('bcrypt');
+
+
 var index = require('./routes/index');
 var users = require('./routes/users');
 
@@ -22,14 +30,72 @@ app.set('view engine', 'ejs');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(expressValidator());
 app.use(cookieParser());
 app.use(lessMiddleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
+var options = {
+  host  : process.env.DB_HOST,
+  user  : process.env.DB_USER,
+  password  : process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+};
+
+var sessionStore = new MySQLStore(options);
+
+app.use(session({
+  secret: 'insertarandomstringhere',
+  resave: false,
+  store: sessionStore,
+  saveUninitialized: false,
+  // cookie: { secure: true }
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+app.use(function(req, res, next){
+  res.locals.isAuthenticated = req.isAuthenticated();
+  next();
+});
+
 app.use('/', index);
 app.use('/users', users);
+
+passport.use(new LocalStrategy(
+  {usernameField: 'email',
+  passwordField: 'password'},
+  function(username, password, done) {
+    console.log(username);
+    console.log(password);
+
+    const db = require('./db');
+    db.query('SELECT id, password FROM students WHERE email = (?)', [username], function(err, results, fields){
+      if(err) {done(err)}
+
+      if (results.length === 0){
+        done(null, false);
+      }else{
+
+        const hash = results[0].password.toString();
+        bcrypt.compare(password, hash, function(err, response){
+          if(response === true){
+            return done(null, {user_id: results[0].id});
+          }else {
+            return done(null, false);
+          }
+        });
+      }
+
+
+    });
+
+  }
+));
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,42 @@
 var express = require('express');
 var bcrypt = require('bcrypt');
 const expressValidator = require('express-validator');
+var passport = require('passport');
 var router = express.Router();
 
 
 
-router.get('/', function(req, res){
+router.get('/', authenticationMiddleware(), function(req, res){
+  // console.log(req.user);
+  // console.log(req.isAuthenticated());
   res.render('home', { title: 'Welcome' });
-})
+});
 
 router.get('/login', function(req, res, next) {
-  res.render('login', { title: 'epicLMS - Login' });
+  if(req.isAuthenticated()){
+    res.redirect('/');
+  }else{
+    res.render('login', { title: 'epicLMS - Login' });
+  }
+});
+
+router.post('/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/login'
+}));
+
+router.get('/logout', function(req, res, next){
+  req.logout();
+  req.session.destroy();
+  res.redirect('/');
 });
 
 router.get('/signup', function(req, res, next) {
-  res.render('signup', { title: 'epicLMS - Signup', errors: null });
+  if(req.isAuthenticated()){
+    res.redirect('/');
+  }else{
+    res.render('signup', { title: 'epicLMS - Signup', errors: null });
+  }
 });
 
 const saltRounds = 10;
@@ -31,61 +53,84 @@ router.post('/signup', function(req, res){
   const errors = req.validationErrors();
 
   if(errors){
-    console.log(`errors: ${JSON.stringify(errors)}`);
+    // console.log(`errors: ${JSON.stringify(errors)}`);
     res.render('signup', { title: 'epicLMS - Signup', errors: errors });
   }else{
     console.log(req.body);
-  var fname = req.body.fname;
-  var lname = req.body.lname;
-  var password = req.body.password;
-  var email = req.body.email;
-  var dept = req.body.dept;
-  var semester = req.body.semester;
+    var fname = req.body.fname;
+    var lname = req.body.lname;
+    var password = req.body.password;
+    var email = req.body.email;
+    var dept = req.body.dept;
+    var semester = req.body.semester;
 
 
-  const db = require('../db.js');
+    const db = require('../db.js');
 
-  if(dept === 'Computer Science'){
-    dept = 1;
-  }else if(dept === 'Mechanical Engineering'){
-    dept = 2;
-  }else if(dept === 'Electrical Engineering'){
-    dept = 3;
-  }else if(dept === 'BBA'){
-    dept = 4;
-  }
-  var usertype = 1;
-  if(req.body.usertype === 'teacher') usertype = 2;
+    if(dept === 'Computer Science'){
+      dept = 1;
+    }else if(dept === 'Mechanical Engineering'){
+      dept = 2;
+    }else if(dept === 'Electrical Engineering'){
+      dept = 3;
+    }else if(dept === 'BBA'){
+      dept = 4;
+    }
+    var usertype = 1;
+    if(req.body.usertype === 'teacher') usertype = 2;
 
-  if(usertype === 2) semester = null;
+    if(usertype === 2) semester = null;
 
-  var pic = '';
-  if(req.body.pic === '') pic = 'default.jpg';
-  else pic = req.body.pic;
+    var pic = '';
+    if(req.body.pic === '') pic = 'default.jpg';
+    else pic = req.body.pic;
 
-  var sqlArr = [fname, lname, email, dept, pic, usertype, semester];
+    var sqlArr = [fname, lname, email, dept, pic, usertype, semester];
 
-  
-  db.query('INSERT INTO students(first_name, last_Name, email, dept, pic, user_type, semester) VALUES (?, ?, ?, ?, ?, ?, ?)', sqlArr, function (error, results, fields) {
-    if (error) throw error;
-    console.log("Inserted " + fname + " " + lname);
-  });
-  
-
-  bcrypt.hash(password, saltRounds, function(err, hash) {
-    db.query('UPDATE students SET password = (?) WHERE email = (?)', [hash, email], function (error, results, fields) {
+    
+    db.query('INSERT INTO students(first_name, last_Name, email, dept, pic, user_type, semester) VALUES (?, ?, ?, ?, ?, ?, ?)', sqlArr, function (error, results, fields) {
       if (error) throw error;
-      console.log("Inserted hashed password.");
+      // console.log("Inserted " + fname + " " + lname);
     });
-  });
+    
 
-  console.log("Signed up. Redirecting...");
-  res.redirect('/login');
+    bcrypt.hash(password, saltRounds, function(err, hash) {
+      db.query('UPDATE students SET password = (?) WHERE email = (?)', [hash, email], function (error, results, fields) {
+        if (error) throw error;
+        // console.log("Inserted hashed password.");
+        // console.log("Signed up. Redirecting...");
+
+        db.query('SELECT LAST_INSERT_ID() as user_id', function(error, results, fields){
+          if (error) throw error;
+
+          const user_id = results[0];
+
+          // console.log(user_id);
+          req.login(user_id, function(err){
+            res.redirect('/');
+          });
+        });
+
+      });
+    });
   }
-
-  
-  
 });
 
+passport.serializeUser(function(user_id, done) {
+  done(null, user_id);
+});
+
+passport.deserializeUser(function(user_id, done) {
+  done(null, user_id);
+});
+
+function authenticationMiddleware () {  
+	return (req, res, next) => {
+		// console.log(`req.session.passport.user: ${JSON.stringify(req.session.passport)}`);
+
+	    if (req.isAuthenticated()) return next();
+	    res.redirect('/login')
+	}
+}
 
 module.exports = router;
